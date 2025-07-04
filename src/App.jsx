460d69850387237e1b6787460bb840028ca16434
Zubair  Farooq <zubair.farooq@muztech.io>
2025-07-04 11:18:36 +0500

    footer edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Navbar from "./pages/navbar";
 import TaskList from "./pages/TaskList";
+import Footer from "./pages/Footer";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <TaskList />
+      <Footer />
     </div>
   );
 };
